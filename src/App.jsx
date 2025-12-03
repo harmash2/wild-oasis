52745ebf084779264ee977ae5f44ec1411e312c4
Dmytro Harmash <harmash2@gmail.com>
2025-12-03 21:59:56 +0100

    day two. starter files, styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import styled from "styled-components"
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from './ui/Button';
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: tomato;
+`;
+
+const StyledApp = styled.div`
+  background-color: #f0d198;
+  padding: 20px;
+  height: 100vh;
+`;
 function App(){
   return (
-    <div>
-      Hello, World!
-    </div>
+    <>
+    <GlobalStyles/>
+    <StyledApp>
+      <H1>
+        Hello, World with Styled!
+      </H1>
+      <Button onClick={() => alert('pink button')}>pink button</Button>
+      <Button onClick={() => alert('pink button')}>purple button</Button>
+      <Input placeholder="number of guests" ttype='number'/>
+    </StyledApp>
+    </>
   )
 }
 
